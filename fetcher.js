a08c6ba322809022699a65a20d1fdb0fb74ff2a4
Sacha Schmid <schmid.sacha@gmail.com>
2016-12-12 16:04:43 +0100

    Allow multiple languages to be fetched, add error handling

--- fetcher.js
+++ fetcher.js
@@ -1,52 +1,70 @@
 const axios = require('axios')
 const fs = require('fs')
 const csv = require('json2csv')
+const chalk = require('chalk')
 
 const base = 'https://api.github.com'
+const langs = process.argv[2].split(',')
 
 let nodes = []
 let edges = []
 
-axios.get(`${base}/search/repositories?q=language:${process.argv[2]}&sort=stars`)
-  .then(res => {
-    const rps = res.data.items.slice(0, 30)
-    const requests = []
+const fetch = () => {
+  const lang = langs.shift()
 
-    rps.forEach(r => {
-      nodes.push({
-        Id: r.id,
-        Label: r.name,
-        size: r.size,
-        type: 'repo'
-      })
+  console.log(`Fetching repositories for language '${lang}'.`)
+  console.log('================================================================================\n')
 
-      console.log(`Added repo '${r.name}'`)
+  axios.get(`${base}/search/repositories?q=language:${lang}&sort=stars`)
+    .then(res => {
+      const rps = res.data.items.slice(0, 60)
+      const requests = []
 
-      requests.push(axios.get(r.contributors_url)
-        .then(resp => {
-          resp.data.forEach(c => {
-            nodes.push({
-              Id: c.id,
-              Label: c.login,
-              type: 'user'
-            })
+      rps.forEach(r => {
+        nodes.push({
+          Id: r.id,
+          Label: r.name,
+          size: r.size,
+          type: 'repo'
+        })
 
-            edges.push({
-              source: c.id,
-              target: r.id,
-              weight: c.contributions
+        requests.push(axios.get(r.contributors_url)
+          .then(resp => {
+            resp.data.forEach(c => {
+              nodes.push({
+                Id: c.id,
+                Label: c.login,
+                type: 'user'
+              })
+
+              edges.push({
+                source: c.id,
+                target: r.id,
+                weight: c.contributions
+              })
+
+              console.log(`${chalk.yellow('·')} Added repo '${r.name}'.`)
             })
           })
-
-          console.log(`Added contributors and edges for repo '${r.name}'`)
-        }))
-    })
-
-    axios.all(requests)
-      .then(() => {
-        fs.writeFile('data/edges.csv', csv({ data: edges }), () => {})
-        fs.writeFile('data/nodes.csv', csv({ data: nodes }), () => {})
-
-        console.log('Wrote files')
+        )
       })
-  })
+
+      axios.all(requests)
+        .then(() => {
+          fs.writeFile('data/edges.csv', csv({ data: edges }), () => {})
+          fs.writeFile('data/nodes.csv', csv({ data: nodes }), () => {})
+
+          console.log(`\n${chalk.green('✓')} Wrote files\n`)
+
+          setTimeout(() => langs.length && fetch(), 3600000)
+        })
+        .catch(() => {
+          console.log(`${chalk.red('ERROR')} Could not retrieve data due to rate limiting issue.`)
+        })
+    })
+    .catch(err => {
+      console.log(`${chalk.red('ERROR')} Could not retrieve data due to rate limiting issue.`)
+    })
+}
+
+fetch()
